5a446d412726276e22782e977f83ce90901a2ebc
Hidran Arias <hidran@gmail.com>
2021-06-21 12:27:44 +0200

    adds ngfor to users template

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,6 +9,46 @@ import { Component } from '@angular/core';
     'users.component.css'
   ]
 })
+
 export class UsersComponent {
+  title = 'Users'
+  public users = [
+    {
+      name: 'Hidran1',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran2',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran3',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    },
+    {
+      name: 'Hidran4',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+    }
+  ];
 
 }
