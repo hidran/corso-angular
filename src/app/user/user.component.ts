5ed6664549e3663b62e47315617925b1bda78395
Hidran Arias <hidran@gmail.com>
2021-06-24 01:32:28 +0200

    pass data from users component to user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
+  selector: 'tr[app-user]',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
+  @Input('user-data') user: any;
   constructor() { }
 
   ngOnInit(): void {
