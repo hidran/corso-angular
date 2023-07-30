3f8a5e084e8fa14c91a81ba444915dca7377888e
Hidran <hidran@gmail.com>
2023-07-31 00:49:19 +0200

    signup form. Formbuilder

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup = new FormGroup({});
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+    private router: Router) {
 
   }
   ngOnInit(): void {
@@ -28,5 +30,6 @@ export class LoginComponent implements OnInit {
     console.log(this.loginForm)
     const { email, password } = this.loginForm.value;
     this.auth.signIn(email, password);
+    this.router.navigate(['/']);
   }
 }
